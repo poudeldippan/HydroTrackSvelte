ad59545e8292e1d85b8d6e6aa50736396af8b27f
Dippan Poudel <poudeldippan2004@gmail.com>
2026-07-06 09:12:02 +0545

    Use dynamic private environment variables for DATABASE_URL

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,10 +1,10 @@
-import { DATABASE_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
 const pool = new pg.Pool({ 
-  connectionString: DATABASE_URL,
+  connectionString: env.DATABASE_URL,
   ssl: true
 });
 const adapter = new PrismaPg(pool);
