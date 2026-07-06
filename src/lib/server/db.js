bd756e8be9ccbb79af87beb643e4852329198a4a
Dippan Poudel <poudeldippan2004@gmail.com>
2026-07-06 09:09:25 +0545

    Switch Prisma schema and client to Postgresql (Neon)

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,8 +1,12 @@
+import { DATABASE_URL } from '$env/static/private';
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
-const adapter = new PrismaBetterSqlite3({
-    url: 'file:./dev.db'
+const pool = new pg.Pool({ 
+  connectionString: DATABASE_URL,
+  ssl: true
 });
+const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
